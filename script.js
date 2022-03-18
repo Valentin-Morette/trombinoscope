488f16421e1fafb1ee061856d3ae901259cee58f
MORETTE Valentin <morette.valentin@gmail.com>
2022-03-18 15:52:29 +0100

    random + pointer

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-const arr = ["Théo","Quentin","Louis","Jeffrey","Alexandra","Simon","Jaafar","Valentin"];
+const arr = ["theo","quentin","louis","jeffrey","alexandra","simon","jaafar","valentin"];
 const randomIndex = Math.floor(Math.random()*arr.length);
 const randomValues = arr[randomIndex];
 console.log(randomValues);
+
+const randomButton = document.querySelector('.randomlink');
+randomButton.addEventListener('click', function () {
+    randomButton.href = "#" + randomValues;
+    });
+randomButton.addEventListener('mouseover', function () {
+    randomButton.style.cursor = "pointer";
+    });
