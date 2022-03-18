dedcc78bae3529ef729b8149026a1ff025b86a3f
MORETTE Valentin <morette.valentin@gmail.com>
2022-03-18 14:24:22 +0100

    random fonction + id name

---
+++ script.js
@@ -0,0 +1,4 @@
+const arr = ["Théo","Quentin","Louis","Jeffrey","Alexandra","Simon","Jaafar","Valentin"];
+const randomIndex = Math.floor(Math.random()*arr.length);
+const randomValues = arr[randomIndex];
+console.log(randomValues);
