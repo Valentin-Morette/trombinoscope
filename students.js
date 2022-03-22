1288399ca2c674c5cf261167106969341f726092
MORETTE Valentin <morette.valentin@gmail.com>
2022-03-22 20:49:08 +0100

    create students.js

---
+++ students.js
@@ -0,0 +1,115 @@
+class Student{
+    constructor (name, firstname, age, country, whiteimg, redimg, skills, desc, linkedin, github) {
+        this.name = name;
+        this.firstname = firstname;
+        this.age = age;
+        this.country = country;
+        this.whiteimg = whiteimg;
+        this.redimg = redimg;
+        this.skills = skills;
+        this.desc = desc;
+        this.linkedin = linkedin;
+        this.github = github;
+    }
+}
+
+const student = [
+    new Student(
+        "Alexandra",
+        "De Brito",
+        "30",
+        "Reims",
+        "WI",
+        "RI",
+        "Skills",
+        "description",
+        "https://www.linkedin.com/in/alexandrardrgs/",
+        "https://github.com/Alexandra-Rdrgs"
+    ),
+    new Student(
+        "Jeffrey",
+        "Rigaux",
+        "22",
+        "Reims",
+        "WI",
+        "RI",
+        "Skills",
+        "description",
+        "https://www.linkedin.com/in/alexandrardrgs/",
+        "https://github.com/Alexandra-Rdrgs"
+    ),
+    new Student(
+        "Quentin",
+        "Samuel",
+        "22",
+        "Reims",
+        "WI",
+        "RI",
+        "Skills",
+        "description",
+        "https://www.linkedin.com/in/alexandrardrgs/",
+        "https://github.com/Alexandra-Rdrgs"
+    ),
+    new Student(
+        "Théo",
+        "Depelsemacker",
+        "24",
+        "Reims",
+        "WI",
+        "RI",
+        "Skills",
+        "description",
+        "https://www.linkedin.com/in/alexandrardrgs/",
+        "https://github.com/Alexandra-Rdrgs"
+    ),
+    new Student(
+        "Louis",
+        "Dufourmantelle",
+        "23",
+        "Reims",
+        "WI",
+        "RI",
+        "Skills",
+        "description",
+        "https://www.linkedin.com/in/alexandrardrgs/",
+        "https://github.com/Alexandra-Rdrgs"
+    ),
+    new Student(
+        "Simon",
+        "Duc",
+        "33",
+        "Reims",
+        "WI",
+        "RI",
+        "Skills",
+        "description",
+        "https://www.linkedin.com/in/alexandrardrgs/",
+        "https://github.com/Alexandra-Rdrgs"
+    ),
+    new Student(
+        "Valentin",
+        "Morette",
+        "27",
+        "Rethel",
+        "WI",
+        "RI",
+        "Skills",
+        "description",
+        "https://www.linkedin.com/in/alexandrardrgs/",
+        "https://github.com/Alexandra-Rdrgs"
+    ),
+    new Student(
+        "Jaafar",
+        "Cherif",
+        "33",
+        "Reims",
+        "WI",
+        "RI",
+        "Skills",
+        "description",
+        "https://www.linkedin.com/in/alexandrardrgs/",
+        "https://github.com/Alexandra-Rdrgs"
+    )    
+]
+
+export default students;
